73387884b438e1e469c6a03f1a1115d3b616adb3
Alexei Pancratov <drivergear@gmail.com>
2021-07-28 23:16:42 -0400

    Added copy, isEmpty, concat functions

--- contracts/StringsConsumer.sol
+++ contracts/StringsConsumer.sol
@@ -6,24 +6,33 @@ import "./StringsLib.sol";
 
 contract StringsClient {
     using strings for *;
-    
-    function testToSpan() public pure returns(strings.span memory) {
+
+    function testToSpan() public pure returns (strings.span memory) {
         string memory str = "test123";
-        
+
         return str.toSpan();
     }
-    
-    function testGetSlice() public pure returns(strings.span memory) {
+
+    function testGetSlice() public pure returns (strings.span memory) {
         string memory str = "abcdef";
         strings.span memory strSpan = str.toSpan();
-        
+
         return strSpan.getSlice(2, 5);
     }
-    
-    function testToString() public pure returns(string memory) {
+
+    function testToString() public pure returns (string memory) {
         string memory str = "abcdef";
         strings.span memory strSpan = str.toSpan();
-        
+
         return strSpan.toString();
+    }
+
+    function testConcat() public pure returns (string memory) {
+        strings.span memory str1 = "hello, ".toSpan();
+        strings.span memory str2 = "world!".toSpan();
+
+        strings.span memory concatenated = str1.concat(str2);
+
+        return concatenated.toString();
     }
 }
--- contracts/StringsLib.sol
+++ contracts/StringsLib.sol
@@ -4,44 +4,89 @@ pragma solidity ^0.8.4;
 
 library strings {
     struct span {
-        uint ptr;
-        uint length;
+        uint256 ptr;
+        uint256 length;
     }
-    
-    function toSpan(string memory str) internal pure returns(span memory) {
-        uint ptr;
+
+    function toSpan(string memory str) internal pure returns (span memory) {
+        uint256 ptr;
         assembly {
             // skip string length value (32 bytes)
             ptr := add(str, 0x20)
         }
         return span(ptr, bytes(str).length);
     }
-    
-    function getSlice(span memory str, uint start, uint end) internal pure returns(span memory) {
-        uint newPointer;
-        uint oldPointer = str.ptr;
+
+    function getSlice(
+        span memory str,
+        uint256 start,
+        uint256 end
+    ) internal pure returns (span memory) {
+        uint256 newPointer;
+        uint256 oldPointer = str.ptr;
         assembly {
             newPointer := add(oldPointer, start)
         }
         return span(newPointer, end - start + 1);
     }
-    
-    function toString(span memory str) internal pure returns(string memory) {
-        string memory result = new string(str.length);
-        uint srcPtr = str.ptr;
-        uint destPtr;
+
+    function copy(span memory str) internal pure returns (span memory) {
+        return span(str.ptr, str.length);
+    }
+
+    function isEmpty(span memory str) internal pure returns (bool) {
+        return str.length == 0;
+    }
+
+    function concat(span memory str1, span memory str2)
+        internal
+        pure
+        returns (span memory)
+    {
+        string memory tempStr = new string(str1.length + str2.length);
+        uint256 srcPtr = str1.ptr;
+        uint256 destPtr;
         assembly {
-            destPtr := add(result, 0x20)
+            destPtr := add(tempStr, 0x20)
         }
-        
-        for (uint i = 0; i < str.length; i++) {
+
+        for (uint256 i = 0; i < str1.length; i++) {
             assembly {
                 mstore(destPtr, mload(srcPtr))
                 srcPtr := add(srcPtr, 1)
                 destPtr := add(destPtr, 1)
             }
         }
-        
+
+        srcPtr = str2.ptr;
+
+        for (uint256 i = 0; i < str2.length; i++) {
+            assembly {
+                mstore(destPtr, mload(srcPtr))
+                srcPtr := add(srcPtr, 1)
+                destPtr := add(destPtr, 1)
+            }
+        }
+
+        return toSpan(tempStr);
+    }
+
+    function toString(span memory str) internal pure returns (string memory) {
+        string memory result = new string(str.length);
+        uint256 srcPtr = str.ptr;
+        uint256 destPtr;
+        assembly {
+            destPtr := add(result, 0x20)
+        }
+
+        for (uint256 i = 0; i < str.length; i++) {
+            assembly {
+                mstore(destPtr, mload(srcPtr))
+                srcPtr := add(srcPtr, 1)
+                destPtr := add(destPtr, 1)
+            }
+        }
+
         return result;
     }
 }
